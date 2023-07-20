273356c499f39a85308dd6583ab5cab11fbd4461
Tobiasnovillo <tobinovillosaravia@gmail.com>
2023-07-20 12:26:33 -0300

    modificaciones

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -7,26 +7,32 @@
  */
 function cambiarunidades(id,Valor){
     if(isNaN(Valor)){
-        alert("El valor ingresado no existe.")
+        alert("El valor ingresado es invalido. Ingrese valores en " + id + ".");
         document.lasunidades.unid_metro.value= " ";
         document.lasunidades.unid_pie.value= " ";
         document.lasunidades.unid_yarda.value= " ";
         document.lasunidades.unid_pulgada.value=  " ";
-    }
-    else if (id=="metro"){
+
+    }else if (id=="metro"){ 
+
         document.lasunidades.unid_pulgada.value= Valor*39.3701;
         document.lasunidades.unid_pie.value=Valor*3.28084;
         document.lasunidades.unid_yarda.value=Valor*1.0936;
-    }
-    else if(id=="pulada"){
+    }else if(id=="pulgada"){
+        document.lasunidades.unid_metro.value = Valor*0.0254;
+        document.lasunidades.unid_pie.value = Valor*0.0833333;
+        document.lasunidades.unid_yarda.value = Valor*0.0277778;
+        }else if(id=="pie"){
+            document.lasunidades.unid_metro.value = Valor*0.3048;
+            document.lasunidades.unid_yarda.value = Valor*0.333333;
+            document.lasunidades.unid_pulgada.value= Valor*12;
+        }
+        else if(id=="yarda"){
+            document.lasunidades.unid_metro.value = Valor*0.9144;
+            document.lasunidades.unid_pulgada.value= Valor*36;
+            document.lasunidades.unid_pie.value = Valor*3;
+        }
 
-    }
-    else if(id=="pie"){
-
-    }
-    else if(id=="yarda"){
-
-    }
 }
 let cargarlisteners = ()=> {
     document.getElementById ("mycanvas").addEventListener("mousemove", dibujarcuadriculado)
